9ae8d5f760dab9edeb836ed83035349fe4928e21
enzom-uy <enzom.dev@gmail.com>
2022-11-28 12:59:26 -0300

    feat(upload route): add errResponse struct to handle error responses

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module go_cloudinary
 go 1.19
 
 require (
-	github.com/cloudinary/cloudinary-go/v2 v2.2.0 // indirect
+	github.com/cloudinary/cloudinary-go/v2 v2.2.0
+	github.com/go-chi/chi/v5 v5.0.7
+)
+
+require (
 	github.com/creasty/defaults v1.5.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 )
--- src/api/v1/upload/upload.route.go
+++ src/api/v1/upload/upload.route.go
@@ -4,38 +4,70 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"mime/multipart"
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 )
 
-func Upload(w http.ResponseWriter, r *http.Request) {
-	file, header, _ := r.FormFile("img")
-	cldCloudName := r.FormValue("cloudName")
-	cldApiKey := r.FormValue("cloudApiKey")
-	cldApiSecret := r.FormValue("cloudApiSecret")
-	w.Header().Set("Content-Type", "application/json")
+type FormData struct {
+	file      multipart.File
+	cloudName string
+	apiKey    string
+	apiSecret string
+}
 
-	if cldCloudName == "" || cldApiKey == "" || cldApiSecret == "" || file == nil {
+type errResponse struct {
+	Error        string   `json:"error"`
+	ErrorMessage error    `json:"errorMessage,omitempty"`
+	Fields       []string `json:"fields,omitempty"`
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	file, header, _ := r.FormFile("img")
+	if file == nil {
+		err := errResponse{Error: "No image was found in your request."}
 		w.WriteHeader(404)
-		json.NewEncoder(w).Encode("There are some fields missing in the request.")
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	data := FormData{
+		file:      file,
+		cloudName: r.FormValue("cloudName"),
+		apiKey:    r.FormValue("cloudApiKey"),
+		apiSecret: r.FormValue("cloudApiSecret"),
+	}
+
+	// Iterate all fields from data struct and append every empty field inside emptyFields var
+	values := reflect.ValueOf(data)
+	var emptyFields []string
+	types := values.Type()
+
+	for i := 0; i < values.NumField(); i++ {
+		if values.Field(i).String() == "" {
+			emptyFields = append(emptyFields, types.Field(i).Name)
+		}
+		fmt.Println(values.Field(i).String())
+	}
+
+	if len(emptyFields) > 0 {
+		err := errResponse{Error: "There is one or more fields that are empty:", Fields: emptyFields}
+		w.WriteHeader(404)
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 
 	ctx := context.Background()
 	filenameWithoutExtension := strings.Split(header.Filename, ".")[0]
-	cld, _ := cloudinary.NewFromParams(cldCloudName, cldApiKey, cldApiSecret)
+	cld, _ := cloudinary.NewFromParams(data.cloudName, data.apiKey, data.apiSecret)
 	res, err := cld.Upload.Upload(ctx, file, uploader.UploadParams{PublicID: filenameWithoutExtension})
 	fmt.Println(err)
 
-	if err != nil {
-		w.WriteHeader(404)
-		json.NewEncoder(w).Encode("There was an error while trying to upload the image: ")
-		json.NewEncoder(w).Encode(err)
-	}
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(res)
-
 }
